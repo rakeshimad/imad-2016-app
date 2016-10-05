f00bc5c3640fe451d80dd26cad93719bc91a1ca2
rakeshimad <aarpeesworld@gmail.com>
2016-10-05 23:05:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,11 @@
 console.log('Loaded!');
 
-
+//Change the text of the main-text div
 var element = document.getElementById('main-text');
 
 element.innerHTML='New Value';
+
+var img = document.getElementById('img');
+img.onClick = function(){
+    img.style.marginLeft='100px';
+};
