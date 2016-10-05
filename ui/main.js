4c6c81ed55ab083a3866ba1b005eed77d9b71167
rakeshimad <aarpeesworld@gmail.com>
2016-10-05 23:34:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,6 @@ var element = document.getElementById('main-text');
 element.innerHTML='New Value';
 
 var img = document.getElementById('madi');
-img.onClick = function(){
+img.onClick = function () {
     img.style.marginLeft='100px';
 };
