65413fc24d955a5f5d8cfc782d0da33c6397f868
rakeshimad <aarpeesworld@gmail.com>
2016-10-08 19:03:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New Value'
 
 var img = document.getElementById('madi');
-var marginLeft = 0
+var marginLeft = 0;
 function moveRight () {
     marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft + 'px';
